a51fd075432cd8522f0ea4175220905285127cef
thegenghiskahn <stmbkahn2019@gmail.com>
2021-04-12 12:57:26 -0500

    wrote square_list function

--- p.py
+++ p.py
@@ -1,1 +1,8 @@
 ''' This is a project by Bennett Kahn'''
+
+# this is a function to take in a list and squares all the elements
+def square_list(l):
+	for i in range(len(l)):
+		l[i] *= l[i]
+	return l
+print(square_list([1, 2, 3, 4, 5]))
