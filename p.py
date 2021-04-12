6a84571c9dd83b654381a425c1d8f0371bb19be4
thegenghiskahn <stmbkahn2019@gmail.com>
2021-04-12 13:03:59 -0500

    I added a double list function

--- p.py
+++ p.py
@@ -1,1 +1,8 @@
 ''' This is a project by Bennett Kahn'''
+
+# this is a function that takes in a list and doubles every element in it
+def double_list(l):
+	for i in range(len(l)):
+		l[i] *= 2
+	return l
+print(double_list([1,2,3,4,5]))
